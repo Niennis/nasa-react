a4b524de120de029d34e84c203e875c11b21fb6b
Niennis <estefania.osses.v@gmail>
2018-03-16 09:50:49 -0300

    Estilos modificados

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import Login from './components/menu/login/index'
-import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+// import React, { Component } from 'react';
+// import Login from './components/menu/login/index'
+// import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
 
-class App extends Component {
+// class App extends Component {
 
-  render() {
-    return (
-      <div className="app">
-        <Login />
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="app">
+//         <Login />
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
